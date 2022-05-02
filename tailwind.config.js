65b286a7aeb96bc0813e97d92f1278685241fcee
shervan <shervangh@gmail.com>
2022-05-02 23:54:13 +0430

    add screen and color for best perview

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    content: ["./public/*.html"],
+    theme: {
+        screens: {
+            'xxd': '350px',
+
+            'xd': '480px',
+
+            'sm': '576px',
+
+            'mm': '767px',
+
+            'md': '990px',
+
+            'lg': '1440px',
+        },
+        extend: {
+            colors: {
+                'regal-blue': '#232749',
+                'back-input': '#393d5c',
+                'List-nav': '#21273e',
+                'button': '#7366f0',
+                'textColor': '#9fa4dd',
+                'back-i': '#202443',
+                'colorBox': '#7263e9',
+                'footColor': '#292f45',
+            },
+        },
+    },
+    plugins: [],
+}
